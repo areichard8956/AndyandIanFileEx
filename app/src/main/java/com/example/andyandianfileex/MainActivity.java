e4df2e0948c2d69add347283b59bf9599daca227
Rebelian02 <Rebelian02@gmail.com>
2019-05-10 09:46:12 -0500

    edit main activity

--- app/src/main/java/com/example/andyandianfileex/MainActivity.java
+++ app/src/main/java/com/example/andyandianfileex/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.andyandianfileex;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -48,6 +51,22 @@ private ListView listviewFiles;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.createFile){
+        openCreateFileDialog();
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    private void openCreateFileDialog(){View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.create_file_dialog_layout, null);
+    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    builder.setTitle(getText(R.string.create_file));
+    builder.setView(view);
+    builder.setCancelable(false);
+        Dialog dialog = builder.show();
+
+    }
+
+
 }
